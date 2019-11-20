78ce36a18ae554d1b415e19e68c67a629d127b24
jp-2019 <jeannie.wesell@gmail.com>
2019-11-20 08:31:03 -0500

    Finished login component with Willis favicon

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: LoginComponent }
  ];
 
 @NgModule({
