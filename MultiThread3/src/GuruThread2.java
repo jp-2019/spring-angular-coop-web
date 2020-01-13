18fe17b9973456c7c2342ba4bdb7d810a5c1c92d
Kenny Imarah <kenny.o.imarah@gmail.com>
2020-01-12 21:46:06 -0500

    MultiThread

---
+++ MultiThread3/src/GuruThread2.java
@@ -0,0 +1,39 @@
+
+public class GuruThread2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        GuruThread3 threadguru1 = new GuruThread3("guru1");
+        threadguru1.start();
+        GuruThread3 threadguru2 = new GuruThread3("guru2");
+        threadguru2.start();
+    }
+}
+class GuruThread3 implements Runnable {
+    Thread guruthread;
+    private String guruname;
+    GuruThread3(String name) {
+        guruname = name;
+    }
+    @Override
+    public void run() {
+        System.out.println("Thread running" + guruname);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+            System.out.println(guruname);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread has been interrupted");
+            }
+        }
+    }
+    public void start() {
+        System.out.println("Thread started");
+        if (guruthread == null) {
+            guruthread = new Thread(this, guruname);
+            guruthread.start();
+        }
+
+    }
+}
